54b10a07c154e488e25c005da58cc91ad181c000
Jan Lagarden <fam.lagarden@googlemail.com>
2013-12-29 20:08:44 +0100

    bugfixes

--- rpkg.sh
+++ rpkg.sh
@@ -2,51 +2,84 @@
 # Author: Jan Lagarden
 # script to install packages in a way they can be uninstalled easyly
 
-UNISTALLDIR = "/Users/Shared/.rpkg"
-COMMAND = $1
-PKG
-RECIEPT = $(lsbom $(pkgutil --bom $PKG)) 
-UNRECIEPT
+UNISTALLDIR="/Users/Shared/.rpkg"
+COMMAND=$1
+UNRECIEPT=""
 
-if [[ COMMAND == "-h"  || COMMAND == "--help" || COMMAND == "help" ]]
+# chack if argument exists
+function argexists {
+	if [ -z "$1" ]
+	then
+		echo "wrong usage: type  rpkg.sh -h"
+		exit 1
+	fi
+}
+
+# create directory for unistall files if it doesn' t exist
+mkdir -p  "$UNISTALLDIR"
+
+# check for right usage
+argexists $1
+
+# -h --help --help option
+if [[ $COMMAND == "-h"  || $COMMAND == "--help" || $COMMAND == "help" ]]
 then
 	echo "$0 [ -h --help help] 			to display help\n"
 	echo "use $0 [ -i --install install] pkg  	to install pkg\n"
 	echo "use $0 [ -r --remove remove] pkg 		to remove pkg\n"
 	echo "use $0 [ -l --list list] 			to listall possible packages\n"
-
+	
+	exit 0
 fi
 
-if [[ COMMAND == "-l" || COMMAND == "--list" || COMMAND == "list" ]]
+# -l --list list option
+if [[ $COMMAND == "-l" || $COMMAND == "--list" || $COMMAND == "list" ]]
 then
 	for obj in $(ls UNISTALLDIR)
 	do
 		name = $(basename "$obj")
 		echo = "${name%.uninstall}"
 	done
+	exit 0
 fi
 
+# -i --install install option
 if [[ COMMAND == "-i" || COMMAND == "--install" || COMMAND == "install" ]]
 then
-	PKG = $2
+	argexists $2
+	$PKG = "$2"
+	RECIEPT=$(lsbom $(pkgutil --bom $PKG)) 
+	# loop over bom to check fi fiels already exist 
+	for obj in $RECIEPT
+	do
+		if [ -e $obj ]
+		then
+			echo "file $obj allready exists!"		
+		else
+			$UNRECIEPT = $UNRECIEPT + $obj
 
+		fi
+	done
+	# create unistall file
+	$UNRECIEPT > "$UNINSTALLDIR/$PKG.uninstall"
+	exit 0
 fi
 
-for obj in RECIEPT
-do
-	if [ -e $obj ]
-	then
-		echo "file $obj allready exists!"		
-	else
-		UNRECIEPT = $UNRECIEPT + $obj
-
-	fi
-done
-
-if [[ ! -e $UNINSTALLDIR ]] 
+# -r --remove remove option
+if [[ COMMAND == "-r" || COMMAND == "--remove" || COMMAND == "remove" ]]
 then
- mkdir UNISTALLDIR
+	argexists $2
+	$PKG = "$2"
+	RECIEPT=$(lsbom $(pkgutil --bom $PKG)) 
+	$(cat "$UNINSTALLDIR/$PKG.uninstall")  > $UNRECIEPT
+	
+	for obj in $UNRECIEPT
+	do
+		if [ -e $obj ]
+		then
+			continue
+		fi
+		rm -rf $obj
+	done 
+	exit 0
 fi
-
-UNRECIEPT > "$UNINSTALLDIR/$PKG.uninstall"
-
